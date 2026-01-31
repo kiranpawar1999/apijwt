c5b13bd2e26362bc268851038a8f43dd515fb34d
kiranpawar1999 <kiranpawar6519@gmail.com>
2026-01-31 18:19:03 +0530

    update2

--- Router/User.router.js
+++ Router/User.router.js
@@ -2,26 +2,27 @@ import express from 'express';
 import userModel from '../models/user.models.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import  dotEnv from 'dotenv';
+import dotEnv from 'dotenv';
 dotEnv.config();
 
 const router = express.Router();
 
 // REGISTER API
 router.post('/register', async (req, res) => {
-try{
-const {username, email, password} = req.body;
-let exitUser = await userModel.findOne({$or: [{username},{email}]
-});
-if (exitUser) return res.status(404).json({message: 'Username and Email already exist'});
+    try {
+        const { username, email, password } = req.body;
+        let exitUser = await userModel.findOne({
+            $or: [{ username }, { email }]
+        });
+        if (exitUser) return res.status(404).json({ message: 'Username and Email already exist' });
 
-let newPassword = await bcrypt.hash(password, 10);
-const user = new userModel({username, email, password: newPassword})
-const userSave = await user.save();
-res.json(userSave);
-}catch (error) {
- res.status(500).json({message: error.message });
-}
+        let newPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({ username, email, password: newPassword })
+        const userSave = await user.save();
+        res.json(userSave);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 
@@ -30,26 +31,26 @@ res.json(userSave);
 // LOGIN API
 
 router.post('/login', async (req, res) => {
-    try{
-        const {username, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
-         const user = await userModel.findOne({username});
-        if(!user) return res.status(404).json({ message: "User Not Found"});
+        let user = await userModel.findOne({ email });
+        if (!user) return res.status(404).json("User Not Found");
 
-        const userPassword = await bcrypt.compare(password, user.password);
-        if(!password)  return res.status(404).json({message: "Password Are Not Match"});
+        let userPassword = await bcrypt.compare(password, user.password);
+        if (!userPassword) return res.status(404).json("Password Are Not Match");
 
         const jwtToken = jwt.sign(
-            {userId: user._id, username: user.username},
+            { userId: user._id, userName: user.username },
             process.env.JWT_SECRET,
-        {expiresIn : "1h" }  
-          )
-          res.json({jwtToken});
+            { expiresIn: "1h" }
+        )
+        res.json({ jwtToken });
 
 
         // res.json("Login Successfully");
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
 
     }
 });
@@ -57,7 +58,7 @@ router.post('/login', async (req, res) => {
 //LOGOUT API
 
 router.post('/Logout', async (req, res) => {
-    res.json({message: "Loggout Successfully"});
+    res.json({ message: "Loggout Successfully" });
 
 });
 
