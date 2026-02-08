4843db7bbd69411a13cd271d2e2dd2ebab19aa48
kiranpawar1999 <kiranpawar6519@gmail.com>
2026-02-08 14:15:01 +0530

    update new data 4

--- middleware/userJwtAuth.js
+++ middleware/userJwtAuth.js
@@ -1,28 +1,24 @@
 import jwt from 'jsonwebtoken';
 
 const userAuth = async(req, res, next)=>{
-    
     try {
-        let beareHeader= req.headers['Authorization'];
-        console.log(beareHeader);
-        
-        if(typeof beareHeader!= 'undefined'){
-              const token = beareHeader.split(" ")[1];
-             const user = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(token);
+        let beareHeader = req.headers['authorization'];   // lowercase
+
+        console.log("HEADER:", beareHeader);
+
+        if (typeof beareHeader !== 'undefined') {
+            const token = beareHeader.split(" ")[1];
+            const user = jwt.verify(token, process.env.JWT_SECRET);
             req.token = user;
             next();
-            
-
-        }else{
-             res.status(401).json({message: "Token Not Set"})
+        } else {
+            res.status(401).json({ message: "Token Not Set" });
         }
-
     }
     catch (error) {
-        res.status(403).json({message: 'Invalid or Expired Token'})
-
+        res.status(403).json({ message: 'Invalid or Expired Token' });
     }
 }
+
 
 export default userAuth;
