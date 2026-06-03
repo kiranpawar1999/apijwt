27db85b77bd4a7b352ad8846c05d1649ccc26fff
kiranpawar1999 <kiranpawar6519@gmail.com>
2026-06-03 14:09:06 +0530

    backend update for cors issue

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ const limiter = rateLimit({
 
 const app = express();
  app.use(cors({
-  origin: ["http://localhost:5173",
-          "https://candid-naiad-1b20e2.netlify.app/"],
+  origin: "https://candid-naiad-1b20e2.netlify.app/",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
  }));
